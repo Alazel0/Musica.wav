37b9d20cb9473d465dfbb1baafc924f4a4f908dd
Alazel0 <alexandervenegas25@gmail.com>
2022-11-07 18:45:23 -0300

    commint

--- MusicaTorrent/urls.py
+++ MusicaTorrent/urls.py
@@ -20,7 +20,7 @@ from Musica import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Home, name='Home'),
-    path('Registrarse/', views.Musica, name='Musica'),
+    path('Registrarse/', views.Registrarse, name='Registrarse'),
     path('Descargas/', views.Descargas, name='Descargas'),
     path('comentarios/', views.comentarios, name='comentarios'),
     path('logout/', views.cerrar_sesion, name='logout'),
