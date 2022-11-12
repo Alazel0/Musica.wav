def3f0330fac6fa9c022addb3185ec75740ada15
Alazel0 <alexandervenegas25@gmail.com>
2022-11-12 12:42:30 -0300

    commit

--- Musica/static/css/scripts.js
+++ Musica/static/css/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('.category_list .category_item[category="all"]').addClass('ct_item-active');
+    $('.category_item').click(function(){
+        var catMusik = $(this).attr('category');
+
+        $('category_item').removeClass('ct_item-active');
+        $(this).addClass('ct_item-active');
+
+        $('.musik-item').hide();
+    });
 });
